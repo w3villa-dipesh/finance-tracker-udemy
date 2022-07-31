407861ed51b44da46178985190d9c267d407785f
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-07-31 19:45:13 +0530

    Remove UserStock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -5,9 +5,18 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
       stock.save
-      @user_stock = UserStock.create(user:current_user,stock:stock)
-      flash[:notice] = "Stock #{stock.name} was created successfully"
-      redirect_to my_portfolio_path
-    end
+    end  
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
