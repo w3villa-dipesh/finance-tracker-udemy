dfda7898f540e8d9d5b3ba282aa5d039ba9ec567
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-08-01 11:00:41 +0530

    Added First name and Last name

---
+++ db/migrate/20220801040607_add_first_last_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstLastNameToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
