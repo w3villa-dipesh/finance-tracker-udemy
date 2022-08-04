cb7019c1d67017a5377f654bc6c9814594859b3f
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-08-04 22:48:27 +0530

    Added various fields for user location

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,10 @@ class WelcomeController < ApplicationController
   def index
     @country = request.location.country
     @city = request.location.city
-    # @currency = request.currency
+    country_code =  request.location.country_code
+    @currency = country_code.upcase == "IN" ? "INR" : "USD"
     @address = request.location.address
+    @ip_address = request.location.ip
   end
   
 end
