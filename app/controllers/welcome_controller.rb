1b547fd826f8b8905a87fe1b3806a4c62e9cc15e
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-08-04 22:00:31 +0530

    Added user location

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   
   def index
+    @country = request.location.country
+    @city = request.location.city
+    # @currency = request.currency
+    @address = request.location.address
   end
   
 end
